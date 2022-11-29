2bbd0f1648bd993bcd7add9be3cedeca82b3a1a0
mr.lee <anhduc2781997@gmail.com>
2022-11-29 11:57:44 +0700

    delete class not nacessary

--- src/main/java/com/mta/jwt/demo/service/IAuthorizer.java
+++
@@ -1,7 +0,0 @@
-package com.mta.jwt.demo.service;
-
-import org.springframework.security.core.Authentication;
-
-public interface IAuthorizer {
-    boolean authorize(Authentication authentication, String action, Object callerObj);
-}
